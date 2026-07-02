6841d7bd20386d49ae5439df555926c5bfea38f8
Chimera <pixelmcpe1@gmail.com>
2026-07-02 17:11:52 +0800

    fix sqls

--- RetroRoyale/Assets/database_custom.sql
+++ RetroRoyale/Assets/database_custom.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `player` (
   `Id` bigint(20) NOT NULL,
   `Score` bigint(20) NOT NULL,
   `Language` text CHARACTER SET utf8mb4 NOT NULL,
-  `FacebookId` text CHARACTER SET utf8mb4 NOT NULL,
+  `FacebookId` text CHARACTER SET utf8mb4 DEFAULT NULL,
   `Avatar` text CHARACTER SET utf8mb4 NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
